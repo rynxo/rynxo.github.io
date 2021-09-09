f55541e5daa5250e4b6faed4df735781c27ed5c0
rynxo <abimanewz@gmail.com>
2021-09-09 10:05:20 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Afifh Abimanyu</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/icon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/pp.jpg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Afifh Abimanyu</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://www.instagram.com/rfanda.ayu" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="https://www.facebook.com/blangxa" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://www.instagram.com/rynxoo" class="instagram"><i class="bx bxl-instagram"></i></a>
+        </div>
+      </div>
+
+      <nav class="nav-menu">
+        <ul>
+          <li><a href="#hero" class="active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#tentang"><i class="bx bx-user"></i> <span>Tentang Saya</span></a></li>
+          <li><a href="#kemampuan"><i class="bx bx-pencil"></i> <span>Kemampuan</span></a></li>
+          <li><a href="#ringkasan"><i class="bx bx-file-blank"></i> <span>Ringkasan</span></a></li>
+          <li><a href="#kontak"><i class="bx bx-envelope"></i> Kontak Saya</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+      <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Hi, Saya Afifh Abimanyu</h1>
+      <p>Saya Adalah <span class="typed" data-typed-items="Gamer, Bakul Wedang, Calon Komisaris, Calon DRPD BANTUL 2029, Pendukung Puan Menjadi Presiden 2024"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="tentang" class="tentang">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Tentang Saya</h2>
+          <p>Saya adalah seorang anak yang bisa dikatakan sebagai beban keluarga dan hanya bermain komputer seharian tanpa henti, kecuali ketika
+            saya disuruh ibuku untuk menjaga warung saya didepan rumah, saya memiliki ketertarikan dengan coding walaupun saya tau saya sulit memahami
+            tetapi setelah saya menonton youtube di channel WEB PROGRAMMING UNPAS, saya semakin tertarik dengan pembuatan website sederhana.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/pp.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>PELAJAR SMK N 1 BANTUL</h3>
+            <p class="font-italic">
+              Seorang siswa dari SMK N 1 BANTUL yang memiliki jurusan rasan rasan
+            </p>
+            <div class="row">
+              <div class="col-lg-12">
+                <ul>
+                  <li><i class="icofont-rounded-right"></i> <strong>Tanggal Lahir :</strong> 24 February 2004</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Status Hubungan :</strong> Masih dalam tahap pencarian </li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Asal Kota :</strong> Yogyakarta Independent</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Alamat :</strong> Pondok DK 1 Srandakan, Trimutri, Srandakan, Bantul</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Agama :</strong> Islam</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+    <!-- ======= Skills Section ======= -->
+    <section id="kemampuan" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Kemampuan</h2>
+          <p>Saya memiliki beberapa kemampuan dalam mengoding dan juga desain sebuah photo walaupun saya sadari bahwa codingan saya masih begitu dasar tetapi saya memiliki keinginan untuk belajar apa itu coding dengan semangat yang tinggi.</p>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">30%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JavaScript <i class="val">20%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">Berbicara <i class="val">40%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">INKSCAPE <i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Skills Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="ringkasan" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p>Berikut ini adalah perjalanan sekolah saya.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Ringkasan</h3>
+            <div class="resume-item">
+              <h4>Afifh Abimanyu</h4>
+              <p><em>Sejujurnya baru beberapa tahun belakangan ini saya mempelajari PHP, CSS, HTML yang menurut saya adlaah hal yang menarik untuk dipelajari karena
+                  kita berada di zaman modern yang membutuhkan website untuk kebutuhan apapun dan saya tertarik untuk membuat sebuah website.</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Education</h4>
+              <h5>2008 - 2010</h5>
+              <p><em>SD N 2 Srandakan</em></p>
+            </div>
+            <div class="resume-item">
+              <h5>2008 - 2010</h5>
+              <p><em>SMP N 1 Srandakan</em></p>
+            </div>
+            <div class="resume-item">
+              <h5>2008 - 2010</h5>
+              <p><em>SMK N 1 Bantul</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
+              <h5>2010 - 2014</h5>
+              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+              <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius
+                unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart
+                dila</p>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>Senior graphic design specialist</h4>
+              <h5>2019 - Present</h5>
+              <p><em>Experion, New York, NY </em></p>
+              <ul>
+                <li>Lead in the design, development, and implementation of the graphic, layout, and production
+                  communication materials</li>
+                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the
+                  project. </li>
+                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the
+                  design</li>
+                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Graphic design specialist</h4>
+              <h5>2017 - 2018</h5>
+              <p><em>Stepping Stone Advertising, New York, NY</em></p>
+              <ul>
+                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and
+                  advertisements).</li>
+                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="kontak" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
+            in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="" data-aos="fade-in">
+
+          <div class="col-lg-12 d-flex align-items-stretch">
+            <div class="info">
+              <div class="row">
+                <div class="address col-lg-4">
+                  <i class="icofont-google-map"></i>
+                  <h4>Alamat:</h4>
+                  <p>Pondok, Trimurti, Srandakan</p>
+                </div>
+
+                <div class="email col-lg-4">
+                  <i class="icofont-envelope"></i>
+                  <h4>Email:</h4>
+                  <p>Abimanewz@Gmail.com</p>
+                </div>
+
+                <div class="phone col-lg-4">
+                  <i class="icofont-phone"></i>
+                  <h4>Telepon:</h4>
+                  <p>08510*******</p>
+                </div>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+        <span>Copyright &copy; SORAYA WORLD <?= date('Y'); ?></span>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script type="text/javascript">
+    $(document).on('click', 'ul li', function() {
+      $(this).addClass('active').siblings().removeClass('active')
+    })
+  </script>
+
+</body>
+
+</html>
